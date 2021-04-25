5c757d60f10ea94b4769faa20af405efb62a27b0
Imran Khan <imrankhan.in3@gmail.com>
2021-04-25 16:46:32 +0530

    [v1]: Latency and Fault Tolerance with Hystrix

---
+++ customer-service/src/main/java/com/example/customerservice/OrderClientFallback.java
@@ -0,0 +1,13 @@
+package com.example.customerservice;
+
+import java.util.Collections;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderClientFallback implements OrderClient {
+    @Override
+    public Object getOrdersForCustomer(int customerId) {
+        return Collections.emptyList();
+    }
+}
